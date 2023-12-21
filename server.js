09580a90c6b891252295586b5bab35d3b9acf640
Bhupendra Bhatta <bhupendra.bhatta@outcodesoftware.com>
2023-12-21 13:44:51 +0545

    Contact controller updated

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./conflig/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// connectDb();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
